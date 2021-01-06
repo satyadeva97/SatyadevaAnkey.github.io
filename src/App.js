b2e92d7df6d782a08cdec0bd61ab642fb498f3a4
Satyadeva97 <satyadeva.ankey@gmail.com>
2021-01-05 22:13:30 -0500

    Finish About section

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ import CarousalComponent from "./components/carousal/CarousalComponent";
 import TitleComponent from "./components/title/titleComponent";
 import ScrollComponent from "./components/scroll/ScrollComponent";
 import AboutMeComponent from "./pages/About/AboutComponent";
+import {Parallax} from 'react-parallax';
+import Rotate from 'react-reveal/Rotate';
+import Container from "react-bootstrap/Container";
+import background from "./assets/img/parallax/background.webp"
 
 function App() {
-  return (
-    <>
-        <NavbarComponent />
-        <TitleComponent />
-        <CarousalComponent />
-        <AboutMeComponent />
-    </>
-  );
+    return (
+        <>
+            <NavbarComponent/>
+            <TitleComponent/>
+            <CarousalComponent/>
+            <div>
+                <Parallax blur={10} bgImage={background}
+                          bgImageAlt="the cat" strength={200}>
+
+                    <div>
+                        <Container className="container-box rounded">
+                            <Rotate duration={500} Right>
+                                <AboutMeComponent/>
+                            </Rotate>
+                        </Container>
+                    </div>
+                </Parallax>
+            </div>
+
+        </>
+    );
 }
 
 export default App;
--- src/pages/About/AboutComponent.js
+++ src/pages/About/AboutComponent.js
@@ -3,7 +3,11 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import profile from '../../assets/img/profile/Profile.jpg'
+import Button from 'react-bootstrap/Button';
+import profile from '../../assets/img/profile/Profile.png';
+import facebook from '../../assets/img/facebook (1).svg';
+import LightSpeed from 'react-reveal/LightSpeed';
+import Rotate from 'react-reveal/Rotate';
 
 const AboutMeComponent = () => {
     return (
@@ -12,12 +16,13 @@ const AboutMeComponent = () => {
                 <h1 className="pt-3 text-center font-details pb-3">About Me</h1>
                 <Container>
                     <Row className="pt-3 pb-5 align-items-center">
-                        <Col xs={12} md={6}>
+                        <Col xs={12} md={5}>
                             <Row className="justify-content-center mb-2 mr-2 ">
                                 <Image className="profile justify-content-end" alt="profile" src={profile} rounded/>
                             </Row>
                         </Col>
-                        <Col xs={12} md={6}>
+                        <Col xs={12} md={7}>
+                            <Rotate Left>
                             <Row className=" align-items-start p-2 my-details rounded">
                                 Hi there! I am <strong>&nbsp;Satya Deva Ankey.</strong>
                                 <br />I am a Computer Science graduate student at Northeastern University, Boston.
@@ -33,14 +38,44 @@ const AboutMeComponent = () => {
                                 <br />
                                 <br />
                             </Row>
+                        </Rotate>
                             <Row>
 
+                                <Col xs={4} md={2}>
+                                    <a href="https://drive.google.com/file/d/1cze-JMKjGuI1Ox-3N-gfV57OPxdA_u6W/view?usp=sharing">
+                                        <Button variant="outline-secondary">Resume</Button>
+                                    </a>
+                                </Col>
+                                <Col xs={4} md={2}>
+                                    <a href="https://www.linkedin.com/in/satya-deva-ankey">
+                                        <Button variant="outline-primary">LinkedIn</Button>
+                                    </a>
+                                </Col>
+                                <Col xs={4} md={2}>
+                                    <a href="https://github.com/satyadeva97">
+                                        <Button variant="outline-dark">GitHub</Button>
+                                    </a>
+                                </Col>
+                                <Col xs={4} md={2}>
+                                    <a href="https://www.instagram.com/satyadeva97">
+                                        <Button variant="outline-danger">Instagram</Button>
+                                    </a>
+                                </Col>
+                                <Col xs={4} md={2}>
+                                    <a href="https://www.facebook.com/satyadeva.sai">
+                                        <Button variant="outline-primary">Facebook</Button>
+                                    </a>
+                                </Col>
+                                <Col xs={4} md={2}>
+                                    <a href="#contact">
+                                        <Button variant="outline-info">Contact</Button>
+                                    </a>
+                                </Col>
                             </Row>
                         </Col>
                     </Row>
                 </Container>
             </div>
-
         </div>
     )
 }
